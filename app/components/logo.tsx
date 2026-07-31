2dcb89cdc44c6f29d6bc74db407b4975af094c98
Vatsal Shukla <vatsalshukla2001@gmail.com>
2026-07-31 14:57:34 +0530

    jmd botsignal branding updates2

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-
 import Link from "next/link";
 
 import Image from "next/image";
 
-
-
 export default function LogoBrand() {
-
-	
-
 	return (
 		<Link className="font-display-xl text-2xl tracking-tighter text-blue-950  dark:text-primary-fixed-dim flex items-end gap-2 group" href="/">
 			<div className="bg-transparent text-on-primary p-1.5 rounded-full group-hover:rotate-12 transition-transform duration-300">
